e8f7cf5ad8a8b28068b7a844e25ac6786cb2235a
Paul Sheets <paulsheets26@gmail.com>
2016-07-24 16:02:08 -0600

    updated project 1 files

---
+++ specs/Services/match-by-email.spec.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Paul
+ * Date: 7/21/16
+ * Time: 9:07 PM
+ */
--- src/Infrastructure/InMemoryUserRepository.php
+++ src/Infrastructure/InMemoryUserRepository.php
@@ -48,7 +48,7 @@ class InMemoryUserRepository implements UserRepository
     {
         $this->storage[] = $newUser;
 
-        return $this;
+        return $this;  //why returning $this????
     }
 
     /**
